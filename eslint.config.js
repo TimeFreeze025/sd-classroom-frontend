be3deb450fbf0a92f2938ff71261b0e7c11c2922
Dave Emmanuel Nuada <davenuada025@gmail.com>
2026-09-05 18:55:20 +0800

    Implement Cloudinary Upload Widget

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,13 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      "no-unused-vars": [
+        "off",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
     },
-  }
+  },
 );
